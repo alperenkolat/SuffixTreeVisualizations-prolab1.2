5c9aa2198c295526afa9b60ee62fa81c7bc8831c
alperenkolat <73352908+alperenkolat@users.noreply.github.com>
2021-10-29 19:27:05 +0300

    suffixtreecontrol(); revizyon yapıldı
    suffixtreecontrol(); fonksiyonuna revizyon yapıldı

--- prolab2.c
+++ prolab2.c
@@ -1,7 +1,7 @@
 //Beyhan KARADAĞ 190201012 Alperen KOLAT 200201006
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> 
+#include <string.h>
 /*struct suffix_tree_node
 {
     struct suffix_tree_node *child[100];
@@ -27,7 +27,7 @@ int main()
 
     while (1)
     {
-        printf("%s\n\t* * * * * * * * * MENU * * * * * * * * *\n\n","\x1B[0m");
+        printf("%s\n\t* * * * * * * * * MENU * * * * * * * * *\n\n", "\x1B[0m");
         printf("\tKatari icin sonek agaci olusturabilir mi ? \n\t\t\tGormek icin 1\'e basiniz ...\n\n");
         printf("\tKatari icinde p katari geciyor mu ?; \n\t Geciyorsa gectigi ilk pozisyon yeri\n\t Ve kac kez tekrar ettigini  \n\t\t\t gormek icin 2\'ye basiniz...\n\n");
         printf("\tKatari icin  en uzun tekrar eden altkatari bulmak; \n\t Ve kac kez tekrar ettigini \n\t\t\tGormek icin 3\'e basiniz...\n\n");
@@ -39,7 +39,7 @@ int main()
         {
         case 1:
 
-        suffixtreecontrol();
+            suffixtreecontrol();
             break;
         case 2:
 
@@ -51,7 +51,7 @@ int main()
             break;
 
         case 0:
-        printf("%s\n\tPROGRAMDAN CIKIS YAPTINIZ...\n","\x1B[33m");
+            printf("%s\n\tPROGRAMDAN CIKIS YAPTINIZ...\n", "\x1B[33m");
             exit(0);
             break;
 
@@ -64,66 +64,46 @@ int main()
     return 0;
 }
 
-
 void suffixtreecontrol()
 {
     char str[40];
     int str_lentgh = 0;
-    int str_yari_uzunluk = 0;
-    int sonuc = 1;
-    char str_orta[10];
-    char str_1[10];
-    char str_2[10];
-    
-    FILE *dosyaSTR = fopen("string.txt", "r"); //Kullanıcıdan alınmalı
+    int sonuc = 0;
+
+    FILE *dosyaSTR;
+    if ((dosyaSTR = fopen("string.text", "r")) == NULL)
+    {
+        printf("dosya acma hatasi!\n");
+        exit(1);
+    }
+
+    //Kullanıcıdan alınmalı
     fscanf(dosyaSTR, "%s", str);
     str_lentgh = strlen(str);
 
-
-    str_yari_uzunluk = str_lentgh / 2;
-    rewind(dosyaSTR);
-//bu satir sonrasıdan konrol kelime yarısı için yapılır.
-//her sonek için for döngüsü konulup "i" ile farklı uzunluktaki içinde kontrol yapılması gerekir.
-    fgets(str_1, str_yari_uzunluk + 1, dosyaSTR);
-
-    if ((str_lentgh % 2) == 1)
+    for (int i = 0; i < str_lentgh; i++)
     {
-        fgets(str_orta, 2, dosyaSTR);
+        for (int j = 0; j < str_lentgh; j++)
 
+            if (str[i] == str[j]&&i!=j)
+            {
+
+                if (str[i + 1] == str[j + 1])
+                {
+                    sonuc = 1;
+                }
+            }
     }
-
-    fgets(str_2, str_yari_uzunluk + 1, dosyaSTR);
-
-    sonuc = strcmp(str_1, str_2);
-
+    if (sonuc == 1)
+    {
+        printf("%s\n %s katari icin sonek agaci olusturulamaz!\n\n", "\x1B[31m", str);
+    }
     if (sonuc == 0)
     {
-        printf("%s\n %s katari icin sonek agaci olusturulamaz!\n\n","\x1B[31m", str);
-    }
 
-    else
-    {
-        printf("%s\n katari icin sonek agaci olusturulabilir!\n\n","\x1B[34m", str);
+        printf("%s\n katari icin sonek agaci olusturulabilir!\n\n", "\x1B[34m", str);
     }
-
     fclose(dosyaSTR);
-}
 
-
-
-void reverse_string(char *str)
-{
-    char swap;
-    int size = 0;
-    for(;*(str+size) != '\0';size++);
-
-    size -= 1;
-
-    for(int i=0;i<=size/2;i++)
-    {
-        swap = *(str+i);
-        *(str+i) = *(str+size-i);
-        *(str+size-i) = swap;
-    }
     
 }
